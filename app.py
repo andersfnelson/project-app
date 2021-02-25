194f0b0c6e089b65ee4c3825435f973ca9a69f09
Anders Nelson <andersnelson@Anderss-MacBook-Pro-15.local>
2021-02-25 10:57:40 -0600

    can update user type, started courses

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def roles():
 
 @app.route('/users')
 def users():
-    result = engine.execute('SELECT * FROM advising.USER_TBL;').fetchall()
+    result = engine.execute('SELECT a.user_id, a.first_name, a.last_name, a.email, b.role_name FROM advising.USER_TBL a JOIN advising.ROLE_TBL b ON b.role_id = a.role_id;').fetchall()
     return render_template('users.html', data=result)
 
 @app.route('/adduser', methods = ['POST', 'GET'])
@@ -72,10 +72,11 @@ def deluser(id):
 
 @app.route('/edituser/<string:id>', methods = ['POST', 'GET'])
 def edituser(id):
-    sql = 'SELECT * FROM advising.USER_TBL WHERE user_id = \'%s\';' %(id)
+    sql = 'SELECT a.user_id, a.first_name, a.last_name, a.email, b.role_name FROM advising.USER_TBL a JOIN advising.ROLE_TBL b on a.role_id = b.role_id WHERE user_id = \'%s\';' %(id)
     result = engine.execute(sql).fetchall()
+    result2 = engine.execute('SELECT * FROM advising.ROLE_TBL;').fetchall()
     print(result)
-    return render_template('edituser.html', data=result)
+    return render_template('edituser.html', data=result, data2=result2)
 
 @app.route('/delrole/<string:id>', methods = ['POST', 'GET'])
 def delrole(id):
@@ -98,7 +99,14 @@ def commitupdate(id):
         fname = request.form['fname']
         lname = request.form['lname']
         email = request.form['email']
-        query = 'UPDATE advising.USER_TBL SET first_name = \'%s\', last_name = \'%s\', email = \'%s\' WHERE user_id = \'%s\';' % (fname, lname, email, id)
+        role_name = request.form['roletype']
+        # print(role_name)
+        find_role_query = 'SELECT role_id from advising.ROLE_TBL WHERE role_name = \'%s\'' %(role_name)
+        role_id_result = engine.execute(find_role_query).fetchall()
+        # fetchall() method returns a list of lists, so need to index in to find the role id to update
+        raw_id = role_id_result[0][0]
+        # print(raw_id)
+        query = 'UPDATE advising.USER_TBL SET first_name = \'%s\', last_name = \'%s\', email = \'%s\', role_id = \'%s\' WHERE user_id = \'%s\';' % (fname, lname, email, raw_id, id)
         # print(query)
         engine.execute(query)
         return redirect(url_for('users'))
@@ -113,5 +121,23 @@ def commitroleupdate(id):
         engine.execute(query)
         return redirect(url_for('roles'))
 
+
+@app.route('/courses')
+def courses():
+    sql = 'SELECT * from advising.COURSE_TBL'
+    result = engine.execute(sql).fetchall()
+    return render_template('courses.html', data = result)
+
+
+@app.route('/addcourse', methods = ['POST', 'GET'])
+def addcourse():
+    if request.method == 'POST':
+        course_code = request.form['coursecode']
+        query = 'INSERT INTO advising.COURSE_TBL (course_code) VALUES (\'%s\');' % (course_code)
+        engine.execute(query)
+        # print(fname)
+        print(query)
+        return redirect(url_for('courses'))
+    return render_template('addcourse.html')    
 if __name__ == "__main__":
     app.run(debug=True)
